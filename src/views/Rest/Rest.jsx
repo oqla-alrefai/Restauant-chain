f9275393c1dd10066c4cb88dc78a9d24541b3231
KZTahat <kztahat96@gmail.com>
2024-02-11 13:56:50 +0000

    feat (restaurants) server connected

--- src/views/Rest/Rest.jsx
+++ src/views/Rest/Rest.jsx
@@ -1,19 +1,49 @@
 import "./rest.css";
 import { CreateRestaurant, RestaurantCard, RestaurantView } from "./comps";
-import { dummyData } from "./dummyData.js";
-import { BreadCrump, DataModel } from "../../components";
+import { BreadCrump, DataModel, PageLoader, Title } from "../../components";
 import useBreadcrumb from "../../customHooks/useBreadCrump.jsx";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function Rest() {
+  const [isLoading, setIsLoading] = useState(false);
   let [showAddRestaurantModal, setShowAddRestaurantModal] = useState(false);
   const [viewedRestaurant, setViewedRestaurant] = useState(null);
+  const [restaurants, setRestaurants] = useState([]);
 
   // Breadcrumb setup
   const { trail, view, updateBreadcrumb, handleBack } = useBreadcrumb(
     ["Restaurants"],
     "Restaurants"
   );
+
+  useEffect(() => {
+    const source = axios.CancelToken.source();
+    try {
+      const fetchData = () => {
+        setIsLoading(true);
+        axios
+          .get("http://localhost:3001/getallrestaurants")
+          .then((response) => {
+            setRestaurants(response.data);
+          })
+          .catch((err) => {
+            console.log(err.message); /// error handler
+          })
+          .finally(() => {
+            setIsLoading(false);
+          });
+      };
+      
+      !restaurants.length ? fetchData() : null;
+    } catch (error) {
+      console.error("Error fetching projects:", error.message);
+    }
+
+    return () => {
+      source.cancel("request is canceled");
+    };
+  }, []);
 
   const handleRestaurantChoice = (restaurant) => {
     updateBreadcrumb(`/ ${restaurant.restaurantName}`);
@@ -37,13 +67,19 @@ function Rest() {
 
         {view == "Restaurants" ? (
           <div className="restaurant-list">
-            {dummyData.map((ele) => (
-              <RestaurantCard
-                restaurant={ele}
-                key={ele.id}
-                handler={handleRestaurantChoice}
-              />
-            ))}
+            {isLoading ? (
+              <PageLoader />
+            ) : !restaurants.length ? (
+              <Title text="You Have No Restaurants Yet" />
+            ) : (
+              restaurants.map((ele) => (
+                <RestaurantCard
+                  restaurant={ele}
+                  key={ele.id}
+                  handler={handleRestaurantChoice}
+                />
+              ))
+            )}
           </div>
         ) : viewedRestaurant ? (
           <RestaurantView restaurant={viewedRestaurant} />
