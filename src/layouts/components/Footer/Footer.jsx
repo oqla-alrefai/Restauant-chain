9e1b87fb44ffa007223be76a838324c20b4a185b
oqla-alrefai <realrefai.rr@gmail.com>
2024-08-18 18:31:34 +0000

    restauran

--- src/layouts/components/Footer/Footer.jsx
+++ src/layouts/components/Footer/Footer.jsx
@@ -4,8 +4,8 @@ function Footer() {
   return (
     <footer className="main-footer">
       <p>
-        Copy Right <i className="ri-copyright-line"></i> 15-2-2024 Khaled Zayed
-        Tahat. All Rights Reserved
+        Copy Right <i className="ri-copyright-line"></i> 15-2-2024 Okleh Alrefai
+        . All Rights Reserved
       </p>
     </footer>
   );
